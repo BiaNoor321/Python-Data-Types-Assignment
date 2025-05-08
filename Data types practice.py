aa797c1b6a48f8081eb89721c3e7e3bace6deff5
Bia Noor <bianoor77@gmail.com>
2025-05-08 16:03:10 +0500

    Add files via upload

---
+++ Data types practice.py
@@ -0,0 +1,23 @@
+# Integer_Example
+Number_of_students = 70
+Total_classes = 3
+print("Students:",Number_of_students)
+print("Classes:",Total_classes)
+
+# String_Example
+My_name = "Bia_Noor"
+I_am_learning = "Python"
+print("Name:",My_name)
+print("Learnig:",I_am_learning)
+
+# Float_Example
+Percentage = 98.5
+Temperature = 47.9
+print("Percentage:",Percentage)
+print("Temperature:",Temperature)
+
+# Boolean_Example
+Girl_pass = True
+Boy_fail = False
+print("Girl_pass:",Girl_pass)
+print("Boy_fail:",Boy_fail)
